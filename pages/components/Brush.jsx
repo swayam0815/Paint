7f54a9b55076ab569821d58aae1cc8580ceb1f63
ylee862 <damy03121y@gmail.com>
2024-01-21 12:38:21 -0500

    changes to canvas

---
+++ pages/components/Brush.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Brush = () => {
+  function handleBrush() {}
+
+  return <div></div>;
+};
+
+export default Brush;
--- pages/components/Canvas.jsx
+++ pages/components/Canvas.jsx
@@ -3,7 +3,18 @@ import React, { useRef } from "react";
 const Canvas = () => {
   const canvas = useRef(null);
 
-  return <canvas id="canvas"></canvas>;
+  function func() {}
+
+  return (
+    <canvas
+      id="canvas"
+      ref={canvas}
+      onMouseDown={() => func}
+      onMouseUp={() => func}
+      onMouseMove={() => func}
+      onMouseLeave={() => func}
+    ></canvas>
+  );
 };
 
 export default Canvas;
