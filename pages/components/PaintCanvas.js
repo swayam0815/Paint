4943d52104f5f685895f4f035c7c0f57850c79ea
ylee862 <damy03121y@gmail.com>
2024-02-04 20:50:44 -0500

    eraser tool

--- pages/components/PaintCanvas.js
+++ pages/components/PaintCanvas.js
@@ -5,6 +5,7 @@ const Canvas = () => {
   //setting the initial canvas as null
   //using useRef to prevent from constant re-rendering
   const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
 
   //using useState for drawing state
   const [drawing, setDrawing] = useState(false);
@@ -13,6 +14,7 @@ const Canvas = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
+    ctxRef.current = ctx;
 
     //setting the width and the height of the canvas
     canvas.width = 500;
@@ -28,13 +30,14 @@ const Canvas = () => {
   const isDrawing = (e) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
+    ctxRef.current = ctx;
 
     const mouseX = e.nativeEvent.offsetX;
     const mouseY = e.nativeEvent.offsetY;
 
     if (!drawing) {
-      ctx.beginPath();
-      ctx.moveTo(mouseX, mouseY);
+      ctxRef.current.beginPath();
+      ctxRef.current.moveTo(mouseX, mouseY);
     }
 
     setDrawing(true);
@@ -42,6 +45,7 @@ const Canvas = () => {
 
   //when onMouseUp, and stops drawing
   const stopDrawing = () => {
+    ctxRef.current.closePath();
     setDrawing(false);
   };
 
@@ -49,27 +53,42 @@ const Canvas = () => {
   const continueDrawing = (e) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
+    ctxRef.current = ctx;
 
     const mouseX = e.nativeEvent.offsetX;
     const mouseY = e.nativeEvent.offsetY;
 
     if (!drawing) return;
     else {
-      ctx.lineTo(mouseX, mouseY);
-      ctx.stroke();
+      ctxRef.current.lineTo(mouseX, mouseY);
+      ctxRef.current.stroke();
     }
   };
 
+  const setToDrawing = () => {
+    ctxRef.current.globalCompositeOperation = "source-over";
+  };
+
+  const setToErasing = () => {
+    ctxRef.current.globalCompositeOperation = "destination-out";
+  };
+
   return (
-    <canvas
-      id="canvas"
-      ref={canvasRef}
-      onMouseDown={isDrawing}
-      onMouseUp={stopDrawing}
-      onMouseMove={continueDrawing}
-      onMouseOut={stopDrawing}
-      style={{ border: "1px solid black" }}
-    ></canvas>
+    <div>
+      <canvas
+        id="canvas"
+        ref={canvasRef}
+        onMouseDown={isDrawing}
+        onMouseUp={stopDrawing}
+        onMouseMove={continueDrawing}
+        onMouseOut={stopDrawing}
+        style={{ border: "1px solid black" }}
+      ></canvas>
+      <div>
+        <button onClick={setToDrawing}>Draw</button>
+        <button onClick={setToErasing}>Erase</button>
+      </div>
+    </div>
   );
 };
 
